274dc9c01a007eb288305f0cca8292a1a8997fe4
illusionist-osmin <vic2629@naver.com>
2018-09-23 02:37:51 +0900

    Update download_list.py
    user interface?

--- download/download_list.py
+++ download/download_list.py
@@ -3,10 +3,12 @@ from download_youtube import DownloadYoutube
 
 if __name__ == "__main__":
     try:
+        print("Start Downloading")
         with open("urlList.txt", "r") as f:
             urls=f.readlines()
 
         for url in urls:
             DownloadYoutube(url)
+        print("\nDone!")
     except Exception as e:
         print(e)
